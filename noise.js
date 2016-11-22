a8f0127f2fe90785eb505cbcb3f75ca0f29a7704
Karin Kubischta <karin.kubischta@gmail.com>
2016-11-21 17:04:31 -0800

    refactored to not require me to call my keypress method, displays alert if press key other than defined

--- noise.js
+++ noise.js
@@ -11,28 +11,28 @@ var playKey = function(letter) {
 // on button click of each thing, play the sound.
   $('.instrument').on('click', 'button', function(event) {
     var note = $(this).html();
-    console.log(note);
     playKey(note);
   });
 
-// on keypress of each thing, play the sound.
-    var keyletter = function(letter) {
-      $("body").on('keydown', function(event) {
-        if (event.key == letter) {
-          playKey(letter);
-        }
-      });
-    };
+  // on keypress of each thing, play the sound.
+  $("body").on('keypress', function(event) {
+    try { playKey(event.key);
+    }
+    catch(e) {
+      // statements to handle any exceptions
+      alert("acceptable letters are a-g");
+    }
+  }
+);
 
   // call the keypress for each of the keys.
   // currently only works for lower case letter presses.
-  var key_a = keyletter("a");
-  var key_b = keyletter("b");
-  var key_c = keyletter("c");
-  var key_d = keyletter("d");
-  var key_e = keyletter("e");
-  var key_f = keyletter("f");
-  var key_g = keyletter("g");
-
+  // var key_a = keyletter("a");
+  // var key_b = keyletter("b");
+  // var key_c = keyletter("c");
+  // var key_d = keyletter("d");
+  // var key_e = keyletter("e");
+  // var key_f = keyletter("f");
+  // var key_g = keyletter("g");
 
 });
